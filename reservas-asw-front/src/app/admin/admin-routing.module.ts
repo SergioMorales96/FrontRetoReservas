a01576923c4fccac6b0c2e9b269ea881ddb6124a
David Soler <davisorb95@gmail.com>
2021-06-30 18:36:40 -0500

    build(global): configuración rutas módulo admin
    Configuración de rutas hijas para el módulo de administración

--- reservas-asw-front/src/app/admin/admin-routing.module.ts
+++ reservas-asw-front/src/app/admin/admin-routing.module.ts
@@ -1,12 +1,51 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
 
+import { AdminsComponent } from './admins/admins.component';
+import { BranchesComponent } from './branches/branches.component';
+import { DomainsComponent } from './domains/domains.component';
+import { FloorsComponent } from './floors/floors.component';
+import { RoomsComponent } from './rooms/rooms.component';
+import { SchedulesComponent } from './schedules/schedules.component';
 
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'admins',
+        component: AdminsComponent
+      },
+      {
+        path: 'branches',
+        component: BranchesComponent
+      },
+      {
+        path: 'domains',
+        component: DomainsComponent
+      },
+      {
+        path: 'floors',
+        component: FloorsComponent
+      },
+      {
+        path: 'rooms',
+        component: RoomsComponent
+      },
+      {
+        path: 'schedules',
+        component: SchedulesComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
-  declarations: [],
+  exports: [
+    RouterModule
+  ],
   imports: [
-    CommonModule
+    RouterModule.forChild( routes )
   ]
 })
 export class AdminRoutingModule { }
