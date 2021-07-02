85eac5e1ddedb4449cea8ce8bafb55fb231b50f1
Javier Esteban Lopez Peña <jelopez@asesoftware.com>
2021-07-02 15:53:53 -0500

    Modificación reservas component Importar Modelos 3D Piso

--- reservas-asw-front/src/app/reservations/components/scene/scene.component.ts
+++ reservas-asw-front/src/app/reservations/components/scene/scene.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 @Component({
   selector: 'app-scene',
@@ -6,12 +9,54 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class SceneComponent implements OnInit {
+export class SceneComponent {
 
-  constructor() {
-  }
+constructor(){
+  const loader = new GLTFLoader();
+  const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  const scene = new THREE.Scene();
+  const renderer = new THREE.WebGLRenderer();
+  camera.position.z = 2
 
-  ngOnInit(): void {
-  }
+  scene.background = new THREE.Color( 0xffffff );
 
+  renderer.outputEncoding = THREE.sRGBEncoding;
+  renderer.setSize ( window.innerWidth, window.innerHeight);
+  document.body.appendChild( renderer.domElement );
+
+  const controls = new OrbitControls(camera, renderer.domElement)
+
+  loader.load( 'assets/models/18th_floor/18th_floor.gltf', function ( gltf ) {
+
+    scene.add( gltf.scene );
+
+}, undefined, function ( error ) {
+
+	console.error( error );
+
+} );
+
+window.addEventListener('resize', onWindowResize, false)
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    render()
+    
+}
+
+var animate = function () {
+    requestAnimationFrame(animate)
+
+    controls.update()
+
+    render()
+};
+
+function render() {
+    renderer.render(scene, camera)
+}
+animate();
+
+}
 }
