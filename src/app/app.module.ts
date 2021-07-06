1f9f19dfc371ed85f144aaeff7d6ee596a1e3a1f
smmorales <smmorales@asesoftware.com>
2021-07-06 18:31:01 -0500

    Adición del calendario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
-
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
   declarations: [
