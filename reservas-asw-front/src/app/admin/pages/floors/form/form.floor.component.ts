870aacc56e652b8e2f82c84c65fd460ddbe59742
ccruz <ccruz@asesoftware.com>
2021-07-07 16:31:22 -0500

    vista
    se implemento la tabla y el formulario

--- reservas-asw-front/src/app/admin/pages/floors/form/form.floor.component.ts
+++ reservas-asw-front/src/app/admin/pages/floors/form/form.floor.component.ts
@@ -1,16 +1,78 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Floor, Sucursal } from '../../../interfaces/admin.interfaces';
 
 @Component({
-  selector: 'app-add',
+  selector: 'app-floor-form',
   templateUrl: './form.floor.component.html',
   styles: [
   ]
 })
 export class FormFloorComponent implements OnInit {
+  floorForm = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(100)]],
+    maxCapacity: ['', [Validators.required]],
+    numberFloor:['',[Validators.required]],
+    branchId: ['', [Validators.required]],
+    floorId: ['', [Validators.required]],
+  });
+  sucursal: Sucursal[] = [
+    {
+      idSucursal: 1,
+      aforoMaximo: 300,
+      direccion: "BOGOTA",
+      nit: "9000001",
+      nombre: "TORRE SIGMA",
+      nombreEmpresa: "ASESOFTWARE"
+    }
+  ];
+  
+  isEditing: boolean = false;
+  floor!: Floor;
 
-  constructor() { }
+  get formTitle(): string {
+    return this.isEditing ? ( this.floor.nombre ?? 'Editar piso' ) : 'Crear piso';
+  }
+
+  get buttonLabel(): string {
+    return this.isEditing ? 'Actualizar' : 'Crear';
+  }
+  constructor(
+    private ActivatedRoute: ActivatedRoute,
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+      this.ActivatedRoute.params
+      .subscribe(({ id }) => {
+        if ( id ) {
+          this.isEditing = true;
+          console.log( 'floor id', id );
+
+          this.floor = {
+            idPiso:      1,
+            aforoMaximo: 700,
+            idSucursal:  1,
+            nombre: "prueba",
+            numeroPiso: 21,
+            
+          }
+          this.setFloor(this.floor);
+        }
+      });
+  }
+
+  setFloor( floor: Floor ): void {
+    this.floorForm.controls['maxCapacity'].setValue(floor.aforoMaximo);
+    this.floorForm.controls['numberFloor'].setValue(floor.numeroPiso);
+    this.floorForm.controls['branchId'].setValue(floor.idSucursal);
+    this.floorForm.controls['floorId'].setValue(floor.idPiso);
+    this.floorForm.controls['name'].setValue(floor.nombre);
+  }
+
+  saveFloor(): void {
+    console.log('save Floor', this.floorForm.value);
   }
 
 }
