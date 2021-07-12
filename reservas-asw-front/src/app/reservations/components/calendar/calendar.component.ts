d502ffe428643becb3a0bc8678b7b5ec7edb176c
smmorales <smmorales@asesoftware.com>
2021-07-12 16:50:12 -0500

    feature(elements): estilizar calendario
    Se aislaron los estilos del calendar.component.html para centrarlos en el calendar.component.scss

--- reservas-asw-front/src/app/reservations/components/calendar/calendar.component.ts
+++ reservas-asw-front/src/app/reservations/components/calendar/calendar.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
-  styleUrls: ['./calendar.component.scss'] 
+  styleUrls: ['./calendar.component.scss']
 })
 export class CalendarComponent implements OnInit {
 
@@ -14,34 +14,32 @@ export class CalendarComponent implements OnInit {
   ngOnInit(): void {
   }
 
-    //Las fechas en esta lista desactivan los días en el calendario
-    invalidDates: Date[] = [new Date("07/7/2021"), new Date("07/08/2021")];
-    //Las fechas en esta lista colorean la mitad de abajo de los días en el calendario
-    afterNoon: number[] = [5, 10, 15, 20, 25, 30];
-    //Las fechas en esta lista colorean la mitad de arriba de los días en el calendario
-    morning: number[] = [6, 11, 16, 26];
-    //Las fechas en esta lista colorean el día completo de los días en el calendario
-    complete: number[] = [7, 8];
-  
-  
-    afterNoonM(day: number): boolean {
-      return this.afterNoon.includes(day);
-    }
-  
-    morningM(day: number): boolean {
-      return this.morning.includes(day);
-    }
-  
-    completeM(day: number): boolean {
-      return this.complete.includes(day);
-    }
-  
-    monthChange(month: number): void {
-      this.complete = [];
-      this.morning = [];
-      this.afterNoon = [];
-  
-      console.log(month);
-    }
+  //Las fechas en esta lista desactivan los días en el calendario
+  invalidDates: Date[] = [new Date("07/7/2021"), new Date("07/08/2021")];
+  //Las fechas en esta lista colorean la mitad de abajo de los días en el calendario
+  afterNoon: number[] = [5, 10, 15, 20, 25, 30];
+  //Las fechas en esta lista colorean la mitad de arriba de los días en el calendario
+  morning: number[] = [6, 11, 16, 26];
+  //Las fechas en esta lista colorean el día completo de los días en el calendario
+  complete: number[] = [7, 8];
 
+  afterNoonM(day: number): boolean {
+    return this.afterNoon.includes(day);
+  }
+
+  morningM(day: number): boolean {
+    return this.morning.includes(day);
+  }
+
+  completeM(day: number): boolean {
+    return this.complete.includes(day);
+  }
+
+  monthChange(month: number): void {
+    this.complete = [];
+    this.morning = [];
+    this.afterNoon = [];
+
+    console.log(month);
+  }
 }
