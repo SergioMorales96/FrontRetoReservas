a9ac023540e24c2b32aa46734ff1c880ab201e59
Neider Yesid Barreto Alarcon <nbarreto@asesoftware.com>
2021-07-14 11:34:42 -0500

    feat(elements) : servicio capacidad
    Se llama el servicio capacidad desde el back

--- reservas-asw-front/src/app/reservations/services/reservations.service.ts
+++ reservas-asw-front/src/app/reservations/services/reservations.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { DataResponse } from '../interfaces/reservations.interface';
+import { environment } from '../../../environments/environment';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReservationsService {
 
-  constructor() { }
+  private apiUrl: string = `${environment}/reservas`;
+
+  constructor(
+    private http: HttpClient 
+  ) { }
+
+  getCapacity(selectedDate: string, selectedFloor: number): Observable<DataResponse> {
+    const url = `${this.apiUrl}/reservas_aforo_dia/${selectedDate}/${selectedFloor}`;
+    return this.http.get<DataResponse>(url)
+      .pipe(
+        catchError(() => of({ data: 0 }))
+      );
+  } 
 }
