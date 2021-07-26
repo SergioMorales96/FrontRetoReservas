6bb830860ce0422e39f0ddb5a257a3746e664f2d
Fabian Esteban Peña Castillo <fpena@asesoftware.com>
2021-07-26 09:05:37 -0500

    feature (element): Actualización de Sidebar
    Corrección de la vista del Sidebar

--- reservas-asw-front/src/app/shared/sidebar/sidebar.component.ts
+++ reservas-asw-front/src/app/shared/sidebar/sidebar.component.ts
@@ -8,77 +8,12 @@ import { RouteName, RouteFloor } from '../../../utils/enums';
   styles: [
   ]
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent {
 
-  @Input() display: boolean = false;
-  @Output() onHide = new EventEmitter<boolean>();
+  display: boolean = false;
 
-  items: MenuItem[] = [];
-
-  ngOnInit(): void {
-    this.items = [
-      {
-        label: 'Generar una reserva',
-        icon: 'pi pi-times-circle',
-        disabled: true,
-      },
-      {
-        label: 'Mis reservas',
-        icon: 'pi pi-calendar-times',
-        disabled: true,
-      },
-      {
-        label: 'Administración',
-        items: [
-          {
-            label: 'Lista de administradores',
-            icon: 'pi pi-users',
-            routerLink: RouteName.AdminsList,
-            command: () => this.display = false
-          },
-          {
-            label: 'Lista de dominios',
-            icon: 'pi pi-list',
-            routerLink: RouteName.DomainsList,
-            command: () => this.display = false
-          },
-          {
-            label: 'Lista de horarios de puestos de trabajo',
-            icon: 'pi pi-clock',
-            routerLink: RouteName.SchedulesList,
-            command: () => this.display = false
-          },
-          {
-            label: 'Lista de pisos',
-            icon: 'pi pi-list',
-            routerLink: RouteFloor.FloorList,
-            command: () => this.display = false
-          },
-          {
-            label: 'Lista de puestos de trabajo',
-            routerLink: RouteName.WorkstationList,
-            icon: 'pi pi-briefcase',
-            command: () => this.display = false
-          },
-          {
-            label: 'Lista de salas',
-            icon: 'pi pi-list',
-            routerLink: RouteName.RoomsList,
-            command: () => this.display = false
-          },
-          {
-            label: 'Lista de sucursales',
-            icon: 'pi pi-list',
-            routerLink: RouteName.BranchesList,
-            command: () => this.display = false
-          },
-        ]
-      }
-    ]
-  }
-
-  hide( ): void {
-    this.onHide.emit(true);
+  selectButton(activate: boolean):void {
+    this.display = activate;
   }
 
 }
