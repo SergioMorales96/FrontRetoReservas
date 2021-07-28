774b8702c3abc4a9ca75b036f8e2bcec172ba814
Jorge Andrés Lucero Hernández <jlucero@asesoftware.com>
2021-07-28 10:25:34 -0500

    Adding the attendee emails field for create a new Reservation.
    ◉ Were modify the reservation interface for add the new field emailsAsistentes which is an optional row because depends of the tipoReserva field.
    ◉ The toasts  were modified in order to show the correct message.
    ◉ The Confirmation dialog were separated in successful or error Confirmation Dialog.
    ◉ The new field on the test object were added with some emails for test.

--- reservas-asw-front/src/app/reservations/components/reservation/reservation.component.ts
+++ reservas-asw-front/src/app/reservations/components/reservation/reservation.component.ts
@@ -27,7 +27,8 @@ export class ReservationComponent  {
     proyecto:"SEMILLA_2021_2",
     idPuestoTrabajo: 5,
     idRelacion: 1,
-    tipoReserva: "SALA"
+    tipoReserva: "PUESTO",
+    emailsAsistentes: "prueba@gmail.com, con@con.con, testeoeo@asw.xx"
   }
 
   constructor(
@@ -40,17 +41,36 @@ export class ReservationComponent  {
   ){}
 
   addReservation(){
-    this.alertsService.showConfirmDialog({
-      message: `Se ha realizado la reserva con éxito, recuerda que si no se cumplen las reservas, existirá una penalización para poder realizar futuras reservas.`,
-      header: 'Creación de reserva ',
-    }).then(resp => {
       this.reservationService.addReservation(this.reservation)
       .subscribe(
         (reservationResponse: ReservationResponse) => {
-          //this.router.navigateByUrl(RouteName.BranchesList);
-          this.toastService.showToastSuccess({ summary: 'Reserva creada', detail: `Creación exitosa`});
+          if(reservationResponse.status === `OK`){
+          this.alertsService.showConfirmDialog({
+            message: `Se ha realizado la reserva con éxito, recuerda que si no se cumplen las reservas, existirá una penalización para poder realizar futuras reservas.`,
+            header: 'Creación de reserva ',
+          }).then(resp =>{
+            if(resp)
+            this.toastService.showToastSuccess({ summary: 'Reserva creada', detail: `Se creó la reserva exitosamente`});
+            else{
+              return;
+            }
+          }).catch(console.log);
+          
+        }
+          else if(reservationResponse.status === `INTERNAL_SERVER_ERROR`){
+          this.alertsService.showConfirmDialog({
+            message: `Ups... No fue posible crear la reserva :(`,
+            header: 'Error en la creación de la reserva ',
+          }).then(resp =>{
+            if(resp)
+            this.toastService.showToastDanger({ summary: 'Reserva NO creada', detail: `No se pudo crear la reserva`});
+            else{
+              return;
+            }
+          })
+          
+          }
         });
-    });
-   
+        
   }
 }
