6b41116fd90c26ed62ef01609875289a23730ba6
Julian Ortiz <jortizg55863@universidadean.edu.co>
2021-08-17 15:47:52 -0500

    feature (element): Cambio de estilos

--- reservas-asw-front/src/app/shared/sidebar/sidebar.component.ts
+++ reservas-asw-front/src/app/shared/sidebar/sidebar.component.ts
@@ -4,6 +4,7 @@ import { AppState } from '../../app.reducer';
 import { MenuItem } from 'primeng/api';
 import { RouteFloor, RouteName } from '../../../utils/enums';
 import { setDisplay } from '../../reservations/reservation.actions';
+import { GridHelper } from 'three';
 
 @Component({
   selector: 'app-sidebar',
@@ -38,7 +39,20 @@ export class SidebarComponent {
 
   ngOnInit(): void {
     this.store.select('reservation').subscribe(
-      (reservation) => this.display = reservation.display
+      (reservation) => 
+      {
+        this.display = reservation.display;
+        const line = document.getElementById('line_shade');
+        if (this.display) {
+          if (line) {
+            line.style.display = 'flex';
+          }
+        } else {
+          if (line) {
+            line.style.display = 'none';
+          }
+        }
+      }
     );
     this.store.select('reservation').subscribe(
       (reservation) => this.blocked = reservation.blocked
@@ -51,35 +65,43 @@ export class SidebarComponent {
     this.items = [
       {
         label: 'Lista de usuarios',
-        routerLink: ''
+        routerLink: '',
+        icon: 'pi pi-check'
       },
       {
         label: 'Lista de administración',
-        routerLink: 'admin/admins/list'
+        routerLink: 'admin/admins/list',
+        icon: 'pi pi-check'
       },
       {
         label: 'Lista de dominios',
-        routerLink: 'admin/domains/list'
+        routerLink: 'admin/domains/list',
+        icon: 'pi pi-check'
       },
       {
         label: 'Lista de pisos',
-        routerLink: 'admin/floors/list'
+        routerLink: 'admin/floors/list',
+        icon: 'pi pi-check'
       },
       {
         label: 'Lista de salas',
-        routerLink: 'admin/rooms/list'
+        routerLink: 'admin/rooms/list',
+        icon: 'pi pi-check'
       },
       {
         label: 'Lista de sucursales',
-        routerLink: 'admin/branches/list'
+        routerLink: 'admin/branches/list',
+        icon: 'pi pi-check'
       },
       {
         label: 'Lista de puestos de trabajo',
-        routerLink: '/admin/workstations/list'
+        routerLink: '/admin/workstations/list',
+        icon: 'pi pi-check'
       },
       {
         label: 'Lista de horarios',
-        routerLink: 'admin/schedules/list'
+        routerLink: 'admin/schedules/list',
+        icon: 'pi pi-check'
       },
     ];
   }
