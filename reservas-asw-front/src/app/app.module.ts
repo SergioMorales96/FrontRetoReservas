c6decc594d1bf63609c74dad847412053c0e6d5d
wsierra <wsierra@asesoftware.com>
2021-08-20 11:35:34 -0500

    Actualizacion librería msal

--- reservas-asw-front/src/app/app.module.ts
+++ reservas-asw-front/src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { LOCALE_ID,NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import localEs from '@angular/common/locales/es-CO';
-import {registerLocaleData} from '@angular/common';
-registerLocaleData(localEs,'es');
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(localEs, 'es');
 
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
@@ -23,6 +23,14 @@ import { CActiveGuard } from './auth/c-active.guard';
 import { ReservationsModule } from './reservations/reservations.module';
 import { ViewReservationComponent } from './reservations/components/reservation/view-reservation/view-reservation.component';
 import { EditReservationComponent } from './reservations/components/reservation/edit-reservation/edit-reservation.component';
+//import { MsalModule } from '@azure/msal-angular';
+import { PublicClientApplication } from '@azure/msal-browser';
+import { MSAL_CONFIG, MsalAngularConfiguration, MSAL_CONFIG_ANGULAR, MsalService } from '@azure/msal-angular';
+import { Configuration } from 'msal';
+import { HTTP_INTERCEPTORS} from '@angular/common/http';
+//import { InterceptorService } from './http-services/interceptors/interceptor.service';
+
+const isIE = window.navigator.userAgent.indexOf('MSIE ') > -1 || window.navigator.userAgent.indexOf('Trident/') > -1;
 
 @NgModule({
   declarations: [
@@ -36,7 +44,7 @@ import { EditReservationComponent } from './reservations/components/reservation/
     AppRoutingModule,
     PrimeNgModule,
     SharedModule,
-    StoreModule.forRoot( appReducers ),
+    StoreModule.forRoot(appReducers),
     ReservationsModule
   ],
   providers: [
@@ -44,8 +52,56 @@ import { EditReservationComponent } from './reservations/components/reservation/
     MessageService,
     CActiveGuard,
     CLoadService,
-    {provide: LOCALE_ID, useValue:'es-CO'}
+    { provide: LOCALE_ID, useValue: 'es-CO' },
+    /*{
+      provide: HTTP_INTERCEPTORS,
+      useClass: InterceptorService,
+      multi: true
+    },*/
+    {
+      provide: MSAL_CONFIG,
+      useFactory: MSALConfigFactory
+    },
+    {
+      provide: MSAL_CONFIG_ANGULAR,
+      useFactory: MSALAngularConfigFactory
+    },
+    MsalService,
   ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+function MSALConfigFactory(): Configuration {
+  return {
+    auth: {
+      clientId: '473482fb-2ddd-44f1-8332-7185bb10b914',
+      authority: 'https://login.microsoftonline.com/bf208dcb-97e8-4d43-bd72-323680bef25c',
+      validateAuthority: true,
+      redirectUri: 'http://localhost:4200/',
+      postLogoutRedirectUri: 'http://localhost:4200/',
+      navigateToLoginRequestUrl: true
+    },
+    cache: {
+      cacheLocation: 'localStorage',
+      storeAuthStateInCookie: isIE,
+    }
+  };
+}
+
+function MSALAngularConfigFactory(): MsalAngularConfiguration {
+  return {
+    popUp: !isIE,
+    consentScopes: [
+      'user.read',
+      'openid',
+      'profile',
+    ],
+    protectedResourceMap: [
+      ['https://graph.microsoft.com/v1.0/me', ['user.read']]
+    ],
+    extraQueryParameters: {}
+  };
+
+}
+
